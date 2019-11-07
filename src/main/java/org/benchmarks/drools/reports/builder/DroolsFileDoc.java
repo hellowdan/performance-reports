cd84543500c2643f9392e9c1b2eb54882bad76d8
drosa <drosa@redhat.com>
2019-11-07 13:18:02 +0100

    it works according to the inicial requirement: reproduce the old way of generate the same report.
    TO-DO
    1. all tests
    2. fill the old_version values
    3. load and fill the older version values

--- src/main/java/org/benchmarks/drools/reports/builder/DroolsFileDoc.java
+++ src/main/java/org/benchmarks/drools/reports/builder/DroolsFileDoc.java
@@ -1,21 +1,14 @@
 package org.benchmarks.drools.reports.builder;
 
 import com.google.api.services.docs.v1.Docs;
-import com.google.api.services.docs.v1.model.BatchUpdateDocumentResponse;
 import com.google.api.services.docs.v1.model.Request;
 import com.google.api.services.sheets.v4.Sheets;
 import org.benchmarks.drools.reports.data.DroolsProperties;
 import org.benchmarks.reports.builder.FileDoc;
-import org.benchmarks.reports.data.DocElementPosition;
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
-import java.util.Set;
 
 public class DroolsFileDoc extends FileDoc {
 
