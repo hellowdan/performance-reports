212b84421034d0adf25d92edb8e3a62868a12c1b
drosa <drosa@redhat.com>
2021-06-02 17:47:43 +0200

    add SubNetworkBenchmark reporting

--- performance-reports-drools/src/main/java/org/benchmarks/data/turtle/runtime/RuntimeReportRow.java
+++ performance-reports-drools/src/main/java/org/benchmarks/data/turtle/runtime/RuntimeReportRow.java
@@ -6,8 +6,53 @@ public class RuntimeReportRow implements ReportRow {
 
     private String benchmark = "";
     private String fancyName = "";
+    private String joinCount = "";
     private String matchRatio = "";
+    private String nestedAccumulates = "";
+    private String objectsPerSegment = "";
+    private String perSegmentUpdate = "";
+    private String segmentCount = "";
     private String score = "";
+
+    public String getJoinCount() {
+        return joinCount;
+    }
+
+    public void setJoinCount(String joinCount) {
+        this.joinCount = joinCount;
+    }
+
+    public String getNestedAccumulates() {
+        return nestedAccumulates;
+    }
+
+    public void setNestedAccumulates(String nestedAccumulates) {
+        this.nestedAccumulates = nestedAccumulates;
+    }
+
+    public String getObjectsPerSegment() {
+        return objectsPerSegment;
+    }
+
+    public void setObjectsPerSegment(String objectsPerSegment) {
+        this.objectsPerSegment = objectsPerSegment;
+    }
+
+    public String getPerSegmentUpdate() {
+        return perSegmentUpdate;
+    }
+
+    public void setPerSegmentUpdate(String perSegmentUpdate) {
+        this.perSegmentUpdate = perSegmentUpdate;
+    }
+
+    public String getSegmentCount() {
+        return segmentCount;
+    }
+
+    public void setSegmentCount(String segmentCount) {
+        this.segmentCount = segmentCount;
+    }
 
     public String getBenchmark() {
         return benchmark;
@@ -44,7 +89,12 @@ public class RuntimeReportRow implements ReportRow {
     @Override
     public String getUniqueID() {
         return "benchmark=" + this.benchmark +
-                "|matchRatio=" + this.matchRatio;
+                "|joinCount=" + this.joinCount +
+                "|matchRatio=" + this.matchRatio +
+                "|nestedAccumulates=" + this.nestedAccumulates +
+                "|objectsPerSegment=" + this.objectsPerSegment +
+                "|perSegmentUpdate=" + this.perSegmentUpdate +
+                "|segmentCount=" + this.segmentCount;
     }
 
     @Override
@@ -52,7 +102,12 @@ public class RuntimeReportRow implements ReportRow {
         return "RuntimeReportRow{" +
                 "benchmark='" + benchmark + '\'' +
                 ", fancyName=" + fancyName + '\'' +
+                ", joinCount=" + joinCount + '\'' +
                 ", matchRatio='" + matchRatio + '\'' +
+                ", nestedAccumulates=" + nestedAccumulates + '\'' +
+                ", objectsPerSegment=" + objectsPerSegment + '\'' +
+                ", perSegmentUpdate=" + perSegmentUpdate + '\'' +
+                ", segmentCount=" + segmentCount + '\'' +
                 ", score=" + score +
                 '}';
     }
--- performance-reports-drools/src/main/java/org/benchmarks/definitions/turtle/runtime/RuntimeSourceFileColumns.java
+++ performance-reports-drools/src/main/java/org/benchmarks/definitions/turtle/runtime/RuntimeSourceFileColumns.java
@@ -3,7 +3,12 @@ package org.benchmarks.definitions.turtle.runtime;
 import org.benchmarks.definitions.SourceFileColumns;
 
 public enum RuntimeSourceFileColumns implements SourceFileColumns {
-    MATCH_RATIO("Param: matchRatio");
+    JOIN_COUNT("Param: joinCount"),
+    MATCH_RATIO("Param: matchRatio"),
+    NESTED_ACCUMULATES("Param: nestedAccumulates"),
+    OBJECTS_PER_SEGMENT("Param: objectsPerSegment"),
+    PER_SEGMENT_UPDATE("Param: perSegmentUpdate"),
+    SEGMENT_COUNT("Param: segmentCount");
 
     private String column;
 
